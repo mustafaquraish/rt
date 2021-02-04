87443ffcb9058926ff2caf2780f8115d48959726
Mustafa Quraish <mustafa.quraish@mail.utoronto.ca>
2021-02-03 19:27:46 -0500

    add antialiasing (makes occlusion 2x slower)

--- src/integrators/ambient_occlusion.cc
+++ src/integrators/ambient_occlusion.cc
@@ -2,7 +2,7 @@
 #include "core/rt.h"
 #include "time.h"
 
-#define AA_SAMPLES 500
+#define AA_SAMPLES 256
 #define MAX_AA_DIST INFINITY
 
 Colour otrace(Ray &ray, Scene *scene) {
@@ -12,16 +12,13 @@ Colour otrace(Ray &ray, Scene *scene) {
 
   if (dot(ray.d, rec.n) > 0) rec.n = -rec.n;
 
-  double sum = 0;
-  for (int i = 0; i < AA_SAMPLES; i++) {
-    Vec dir = randomVectorCosineHemisphere(rec.n);
-    Ray rr = Ray(rec.p, dir, MAX_AA_DIST);
-    if ( !scene->world->hit(rr, tmp) ) {
-      sum += 1.0;
-    }
+  Vec dir = randomVectorCosineHemisphere(rec.n);
+  Ray rr = Ray(rec.p, dir, MAX_AA_DIST);
+  if ( !scene->world->hit(rr, tmp) ) {
+    return Vec(1);
   }
   
-  return sum / AA_SAMPLES;
+  return Vec(0);
 }
 
 void AmbientOcclusion::render(Scene *scene, int depth) {
@@ -29,27 +26,28 @@ void AmbientOcclusion::render(Scene *scene, int depth) {
   clock_t timeBegin = clock();
 
   int done = 0;
-  #pragma omp parallel for
+  #pragma omp parallel for schedule(dynamic)
   for (int i = 0; i < scene->sx; i++) {
     printf("\rRendering row %d / %d ~ %f", done, scene->sx,
            100 * (float)done / scene->sx);
     fflush(stdout);
     for (int j = 0; j < scene->sy; j++) {
-      // DEBUG = i == 182 && j == 145;
-      Ray ray = scene->cam.getRay(i, j);
-      // if (DEBUG) cout << "init ray:  p: " << ray.p << "  d: " << ray.d <<
-      // endl;
-      Vec col = otrace(ray, scene);
-
-      im.set(i, j, col);
+      Colour col = 0;
+      for (int sample = 0; sample < AA_SAMPLES; sample++) {
+        // DEBUG = i == 182 && j == 145;
+        Ray ray = scene->cam.getRay(i, j);
+        // if (DEBUG) cout << "init ray:  p: " << ray.p << "  d: " << ray.d <<
+        // endl;
+        col += otrace(ray, scene);
+      }
+      im.set(i, j, col / AA_SAMPLES);
     }
-    #pragma omp atomic
     done++;
   }
   clock_t timeEnd = clock();
   double buildTime = (double)(timeEnd - timeBegin) / CLOCKS_PER_SEC;
   printf("\n[+] Rendering completed in %.3fs\n", buildTime);
   cout << endl;
-  im.save("output.ppm");
+  im.save("output.ppm", false);
   return;
 }
