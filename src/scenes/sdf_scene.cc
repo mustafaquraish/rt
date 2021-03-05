8727d2a9d8e20a485700e8d877dabd3017287eae
Mustafa <mustafaq9@gmail.com>
2021-03-04 22:57:34 -0500

    added sierpinksi sdf

--- src/scenes/sdf_scene.cc
+++ src/scenes/sdf_scene.cc
@@ -12,8 +12,8 @@ SCENE(SDF) {
   // params.setWindow(0.2, 0.4, 0.4, 0.6);
 
   // scene->integrator = new DebugShader(params);
-  scene->integrator = new AmbientOcclusion(params);
-  // scene->integrator = new BaseColour(params);
+  // scene->integrator = new AmbientOcclusion(params);
+  scene->integrator = new BaseColour(params);
 
 
   double frameRatio = params.get<double>("frameRatio");
@@ -25,12 +25,23 @@ SCENE(SDF) {
 
   // double R = lerp(frameRatio, 0.0, 4.0);
   // s = new InfiniteSphereSDF(new Lambertian(1));
+  // s->addTextureMap(new SDFTexture(Colour(1), 10));
   // s->Translate(R, R, 0);
   // scene->add(s);
 
+  // s = new SerpinksiSDF(new Lambertian(1));
+  // s->addTextureMap(new SDFTexture(Colour(1), 10));
+  // s->Scale(3.5);
+  // s->RotateY(PI/4);
+  // s->RotateX(PI + PI/4);
+  // s->Translate(0, -1, 0);
+  // scene->add(s);
+
+  double R = lerp(frameRatio, 0.0, TAU);
   s = new MandelBulbSDF(new Lambertian(1));
-  s->addTextureMap(new SDFTexture(Colour(1)));
-  s->RotateX(PI/2);
+  s->addTextureMap(new SDFTexture(Colour(1), 3.5));
+  s->RotateX(PI/2 + R);
+  s->RotateZ(R);
   s->Scale(5);
   scene->add(s);
 
