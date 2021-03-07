30a0b92a07f9680196c8b95457a5bb2c38939f4c
Mustafa Quraish <mustafa.quraish@mail.utoronto.ca>
2021-03-06 19:21:08 -0500

    bvhs now linear

--- src/aggregates/bvh.h
+++ src/aggregates/bvh.h
@@ -5,33 +5,37 @@
 #include "core/aabb.h"
 #include <vector>
 
+struct BVHTree {
+  AABB bounds;
+  bool isLeaf = 0;
+  int axis;
+  int offset, numPrims;
+  BVHTree *bva, *bvb;
+  bool hit(Ray& ray, HitRec& rec, 
+          std::vector<Primitive *>& list);
+};
+
+struct BVHLinear {
+  AABB bounds;
+  union {
+    int primOff;   // leaf
+    int child2Off;  // interior
+  };
+  uint16_t nPrimitives;  // 0 -> interior node
+  uint8_t axis;          // interior node: xyz
+  uint8_t pad[1];        // ensure 32 byte total size
+};
+
 struct BVH : Aggregate {
-    BVH(std::vector<Primitive *>& list, int start=0, int end=-1);
-    
-    ~BVH() { 
-      delete b;
-      delete a;
-    }
-
-    bool isLeaf = 0;
-
-    // Using unions here just to be able to access BVH fields easier later.
-    union {
-      struct { Primitive *a, *b; };
-      struct { BVH *bva, *bvb; };
-    };
-
-    bool hit(Ray& ray, HitRec& rec);
-
-    inline int height() {
-      if (isLeaf) return 1;
-      return max(bva->height(), bvb->height()) + 1;
-    }
-
-    inline int num_nodes() {
-      if (isLeaf) return 1;
-      return bva->num_nodes() + bvb->num_nodes();
-    }
+  BVH(std::vector<Primitive *>& list, int start=0, int end=-1);
+  ~BVH() {};
+  BVHTree *buildBVH(std::vector<Primitive *>& list, int start, int end);
+  int flatten(BVHTree *root);
+  bool hit(Ray& ray, HitRec& rec);
+  
+  BVHTree *bvh;
+  std::vector<BVHLinear> nodes;
+  std::vector<Primitive *> primitives;
 };
 
 #endif // __BVH_H__
