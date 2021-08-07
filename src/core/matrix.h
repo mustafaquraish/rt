e87f4047c7c8390b987c828b1f524ebd0e6052ce
Mustafa Quraish <mustafaq9@gmail.com>
2021-08-07 13:02:27 -0400

    Remove divide-by-w in homogenous matrox-vector mult
    I don't know why but this was breaking refraction... :(

--- src/core/matrix.h
+++ src/core/matrix.h
@@ -247,7 +247,7 @@ inline Vec3 operator*(const Matrix4 &m, const Vec3 &v) {
        m[0][0] * v.x + m[0][1] * v.y + m[0][2] * v.z + m[0][3],
        m[1][0] * v.x + m[1][1] * v.y + m[1][2] * v.z + m[1][3],
        m[2][0] * v.x + m[2][1] * v.y + m[2][2] * v.z + m[2][3]
-  ) / (m[3][0] * v.x + m[3][1] * v.y + m[3][2] * v.z + m[3][3]);
+  );
 }
 
 // This transforms the Vector **WITHOUT** the translation component  
