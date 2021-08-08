5a0e78868094faa3213f5a2250d74e61246cacc8
Mustafa Quraish <mustafaq9@gmail.com>
2021-08-07 23:47:04 -0400

    Refactor Meshes and OBJ loading
    A mesh is now templated based on MeshType::Simple or MeshType::Full.
    Similarly, the OBJ loading is also now templated on the MeshType.
    For `Simple` meshes, only geometry (triangles) are loaded in and
    put into a BVH.
    
    For `Full` meshes (Not implemented yet), the idea is that all the
    sub-meshes from the OBJ file are loaded in separately, and the
    original TriangleMesh<Full>::mesh is just a BVH of the sub-meshes.
    In effect this gives us a 3-tiered system for Full meshes:
    
      - Global Object BVH (contains all objects)
        - Full Mesh BVH (contains sub meshes)
          - Sub Mesh BVH (contains triangles)

--- src/util/obj_loader.cc
+++ src/util/obj_loader.cc
@@ -3,75 +3,98 @@
 #include <stdlib.h>
 
 #include <util/obj_loader.h>
-#include <objects/triangle.h>
+#include <objects/triangle_mesh.h>
+#include <core/definitions.h>
 
+#include <aggregates/bvh.h>
+#include <aggregates/kdtree.h>
+#include <core/definitions.h>
+
+namespace WavefrontOBJ {
 
 /* Quick utility function to check if a string is a prefix of another */
 int prefix(const char *pre, const char *str) {
   return strncmp(pre, str, strlen(pre)) == 0;
 }
 
-std::vector<Primitive *> wavefrontObjLoader(const char *fname) {
-  FILE *f = fopen(fname, "r");
-  if (f == NULL) {
-    fprintf(stderr, "Couldn't open %s. Please check name/path.\n", fname);
-    exit(1);
+template <MeshType Type>
+void load(TriangleMesh<Type> &mesh, const char *filename) {
+  if constexpr (Type == MeshType::Simple) {
+    auto prims = get_triangles(filename);
+    mesh.mesh = new AGGREGATE(prims);
+    return;
   }
-  
-  char buf[128];
+}
+
+struct MeshData {
   std::vector<Vec3> vs;
   std::vector<Vec3> vns;
   std::vector<Vec3> vts;
   std::vector<Primitive *> faces;
+};
+
+void read_faces(const char *buf, MeshData &data) {
+  int v0, v1, v2, n0, n1, n2, t0, t1, t2;
+  Triangle *t;
+
+  #define VS(i) ((i > 0) ? data.vs[i-1] : data.vs[data.vs.size()+i])
+  #define VTS(i) ((i > 0) ? data.vts[i-1] : data.vts[data.vts.size()+i])
+  #define VNS(i) ((i > 0) ? data.vns[i-1] : data.vns[data.vns.size()+i])
+
+  // Vertices only
+  if (sscanf(buf, "f %d %d %d", &v0, &v1, &v2) == 3) {
+    t = new Triangle(VS(v0),  VS(v1),  VS(v2));
+  }
+
+  // Vertices + Normals
+  if (sscanf(buf, "f %d//%d %d//%d %d//%d", 
+             &v0, &n0, &v1, &n1, &v2, &n2) == 6) {
+    t = new Triangle(VS(v0),  VS(v1),  VS(v2),
+                     VNS(n0), VNS(n1), VNS(n2));
+  }
+  
+  // Vertices + Normals + Textures
+  if (sscanf(buf, "f %d/%d/%d %d/%d/%d %d/%d/%d", 
+             &v0, &t0, &n0, &v1, &t1, &n1, &v2, &t2, &n2) == 9) {
+    t = new Triangle(VS(v0),  VS(v1),  VS(v2),
+                     VNS(n0), VNS(n1), VNS(n2),
+                     VTS(t0), VTS(t1), VTS(t2));
+  }
+
+  #undef VS
+  #undef VTS
+  #undef VNS
+
+  data.faces.push_back(t);
+}
+
+std::vector<Primitive *> get_triangles(const char *filename) {
+  FILE *f = fopen(filename, "r");
+  if (f == NULL) {
+    fprintf(stderr, "Couldn't open %s. Please check name/path.\n", filename);
+    exit(1);
+  }
+  
+  char buf[128];
+  MeshData data;
 
   float x, y, z;
   // Store vertices
   while (fgets(buf, 128, f)) {
-    if (prefix("v ", buf)) {
-      sscanf(buf, "v %f %f %f", &x, &y, &z);
-      // std::cout << "Vertex: " << Vec3(x,y,z) << std::endl;
-      vs.push_back(Vec3(x, y, z));
-    }
-    if (prefix("vt ", buf)) {
-      sscanf(buf, "vt %f %f", &x, &y);
-      // std::cout << "TexCod: " << x << " " << y << std::endl;
-      vts.push_back(Vec3(x, y, 0));
-    }
-    if (prefix("vn ", buf)) {
-      sscanf(buf, "vn %f %f %f", &x, &y, &z);
-      // std::cout << "Normal: " << Vec3(x,y,z) << std::endl;
-      vns.push_back(Vec3(x, y, z));
-    }
-
-    if (prefix("f ", buf)) {
-      int v0, v1, v2, n0, n1, n2, t0, t1, t2;
-      Triangle *t;
-
-
-      // Vertices only
-      if (sscanf(buf, "f %d %d %d", &v0, &v1, &v2) == 3) {
-        t = new Triangle(vs[v0-1],  vs[v1-1],  vs[v2-1]);
-      }
-
-      // Vertices + Normals
-      if (sscanf(buf, "f %d//%d %d//%d %d//%d", 
-                 &v0, &n0, &v1, &n1, &v2, &n2) == 6) {
-        t = new Triangle( vs[v0-1],  vs[v1-1],  vs[v2-1],
-                         vns[n0-1], vns[n1-1], vns[n2-1]);
-      }
-      
-      // Vertices + Normals + Textures
-      if (sscanf(buf, "f %d/%d/%d %d/%d/%d %d/%d/%d", 
-                 &v0, &t0, &n0, &v1, &t1, &n1, &v2, &t2, &n2) == 9) {
-        t = new Triangle( vs[v0-1],  vs[v1-1],  vs[v2-1],
-                         vns[n0-1], vns[n1-1], vns[n2-1],
-                         vts[t0-1], vts[t1-1], vts[t2-1]);
-      }
-
-      faces.push_back((Primitive *) t);
-    }
-    
+    if (sscanf(buf, "v %f %f %f", &x, &y, &z))  { data.vs.push_back(Vec3(x, y, z)); }
+    if (sscanf(buf, "vt %f %f %f", &x, &y, &z)) { data.vts.push_back(Vec3(x, y, z)); }
+    if (sscanf(buf, "vn %f %f %f", &x, &y, &z)) { data.vns.push_back(Vec3(x, y, z)); }
+    if (prefix("f ", buf)) { read_faces(buf, data); }
   }
-  printf("[+] Loaded %s; vert=%lu, face=%lu\n", fname, vs.size(), faces.size());
-  return faces;
+  printf("[+] Loaded %s; vert=%lu, face=%lu\n", filename, data.vs.size(), data.faces.size());
+  return data.faces;
+}
+
+std::vector<Primitive *> get_submeshes(const char *filename) {
+  return std::vector<Primitive *>();
+}
+
+template void load(TriangleMesh<Simple> &, const char *);
+template void load(TriangleMesh<Full> &, const char *);
+
 }
