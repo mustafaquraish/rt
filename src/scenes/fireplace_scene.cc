b31a6000efe1b033eb1e63e38f56321e0f9fcab3
Mustafa Quraish <mustafaq9@gmail.com>
2021-08-09 01:42:05 -0400

    Refresh fireplace scene with better camera angle

--- src/scenes/fireplace_scene.cc
+++ src/scenes/fireplace_scene.cc
@@ -6,38 +6,30 @@ SCENE(Fireplace) {
 
   Scene *scene = new Scene();
   
-
-  Vec3 e = Vec3(5.03f, 0.91f, -2.20f);
-  Vec3 g = Vec3(-0.21f, 1.0f, -0.34f) - e;
-
-  // Vec3 e = Vec3(0, 1, 2);
-  // Vec3 g = Vec3(1, 1, 0) - e;
+  Vec3 e = Vec3(5, 20, -50);
+  Vec3 g = RotateYMatrix(PI/4.5) * Vec3(0, 0, 1);
   
   Vec3 up = Vec3(0, 1, 0);
 
-  scene->cam = Camera(e, g, up, 45, params);
+  scene->cam = Camera(e, g, up, 80, params);
 
-  // scene->renderer = new Path(params);
+  scene->renderer = new Path(params);
   // scene->renderer = new DebugShader(params);
   // scene->renderer = new BaseColour(params);
-  scene->renderer = new DirectLighting(params);
+  // scene->renderer = new DirectLighting(params); 
   // scene->renderer = new AmbientOcclusion(params);
   
   Object *s;
 
   s = new TriangleMesh<Full>("assets/obj/fireplace_room/fireplace_room.obj");
+  s->Scale(20);
   scene->add(s);
 
-  // s = new TriangleMesh<Simple>("assets/obj/living_room.obj", new Lambertian(Colour(.95)));
-  // s->Translate(-10, -1, -5);
-  // s->Scale(3);
-  // scene->add(s);
-
-
-  s = new Sphere(new Emitter(Colour(.99, .7, .5) * 30));
-  s->Scale(1, 1, 1);
+  s = new Disc(new Emitter(Colour(.99, .7, .5) * 10));
+  s->RotateX(PI/2);
+  s->Scale(10, 10, 10);
   // s->RotateX(PI/2);
-  s->Translate(5.03f, 2.91f, -2.20f);
+  s->Translate(70, 56, -30);
   scene->add(s);
 
   // scene->world = new BVH(scene->obj_list);
