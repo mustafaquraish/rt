bdf295f0c98b9afb445bed26fcd9991cd113fa37
Mustafa Quraish <mustafaq9@gmail.com>
2021-08-09 12:46:16 -0400

    Update the convert_images script to swap the format
    Now you can pass in an optional argument for the image format,
    and that will be used when converting the images and when changing
    filename references in the .mtl files.
    
    Also changed the `sed` command to be more robust and replace any
    extensions with the given one, not just some hardcoded ones

--- scripts/convert_images.sh
+++ scripts/convert_images.sh
@@ -2,30 +2,44 @@
 if [ -z "$1" ]
 then
   echo "Usage: $0 <directory>"
+  exit 1
 fi
+
+FORMAT=png
+if [ $# -gt 1 ]
+then
+  FORMAT=$2
+fi
+
+function convert_image() {
+  local file=$1
+  echo "Converting $FILE"
+  mogrify -format $FORMAT -quality 100 $file 2>&1 | grep -v "warning"
+  rm $file
+}
 
 for FILE in $(find $1 -type f)
 do
   if file "$FILE" | grep -qE 'image|bitmap'; then
-    if [ "${FILE: -4}" != ".png" ]
+    if [[ "$FILE" != *"$FORMAT" ]]
     then
-      echo "Converting $FILE"
-      mogrify -format png -quality 100 "$FILE"
-      rm "$FILE"
+      convert_image $FILE &
     fi
   elif [ "${FILE: -4}" == ".mtl" ]
   then
-    if $(grep "map_Kd" "$FILE" | grep -q -v ".png")
+    if $(grep "map_Kd" "$FILE" | grep -q -v ".$FORMAT")
     then
         echo "Changing image references in $FILE"
         if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-          sed -i -e "s/jpg/png/g" -e "s/ppm/png/g" "$FILE"
+          sed -i -e "s/\(map_Kd.*\)\.[^.]*$/\1\.$FORMAT/g" "$FILE"
         elif [[ "$OSTYPE" == "darwin"* ]]; then
           # MacOS / BSD sed needs some special syntax for in place editing
-          sed -i '' -e "s/jpg/png/g" -e "s/ppm/png/g" "$FILE"
+          sed -i '' -e "s/\(map_Kd.*\)\.[^.]*$/\1\.$FORMAT/g" "$FILE"
         fi
     fi
   fi
 done
+
+wait
 
 echo "Done."
