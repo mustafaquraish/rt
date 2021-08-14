a4369573117f68a7fcc3de294357b140025fba38
Mustafa Quraish <mustafaq9@gmail.com>
2021-08-14 01:38:26 -0400

    Scenes: Update CSG scene with CSG from Wikipedia
    Cool-looking thing with all 3 CSG operations involved :)

--- src/scenes/csg_scene.cc
+++ src/scenes/csg_scene.cc
@@ -6,17 +6,19 @@ SCENE(csg) {
 
   Scene *scene = new Scene();
   
-  Vec3 e = Vec3(0, 0, -15);
-  Vec3 g = -e;
+  Vec3 eye = Vec3(0, 0, -15);
+  Vec3 g = -eye;
   Vec3 up = Vec3(0, 1, 0);
-  scene->cam = Camera(e, g, up, 70, params);
+  scene->cam = Camera(eye, g, up, 70, params);
 
-  // scene->renderer = new Path(params);
-  scene->renderer = new DebugShader(params);
+  scene->renderer = new Path(params);
+  // scene->renderer = new DebugShader(params);
   // scene->renderer = new DirectLighting(params);
   // scene->renderer = new BaseColour(params);
   
   Object *s;
+
+  float frameRatio = params.get<float>("frameRatio");
 
   // Right
   s = new Sphere(new Lambertian(Colour(.25, .25, .75)));
@@ -28,7 +30,6 @@ SCENE(csg) {
   // Left
   s = new Sphere(new Lambertian(Colour(.75, .25, .25)));
   s->Scale(500, 500, 500);
-  s->RotateY(-PI / 2);
   s->Translate(-510, 0, 5);
   scene->add(s);
 
@@ -41,36 +42,52 @@ SCENE(csg) {
   // Bottom
   s = new Sphere(new Lambertian(Colour(.75, .75, .75)));
   s->Scale(500, 500, 500);
-  s->RotateX(-PI / 2);
   s->Translate(0, -510, 5);
   scene->add(s);
 
   // Top
   s = new Sphere(new Lambertian(Colour(.75, .75, .75)));
   s->Scale(500, 500, 500);
-  s->RotateX(PI / 2);
   s->Translate(0, 510, 5);
   scene->add(s);
 
 
-  Object *a, *b;
+  /**
+   * Creating the Diagram from wikipedia page:
+   * https://en.wikipedia.org/wiki/Constructive_solid_geometry
+   */
 
-  double f = params.get<float>("frameRatio");
+  Object *a, *b, *c, *d, *e;
+
+  a = new Cube(new Lambertian(Colour(1, 0.5, 0.5)));
+  a->Scale(3);
   
-  a = new Sphere(nullptr);
-  a->Scale(3.75, 3.75, 3.75);
-  a->Translate(0, -4, 4.5);  
-  // scene->add(a);
+  b = new Sphere(new Lambertian(Colour(0, 0, 1)));
+  b->Scale(4);
 
-  b = new Cube(nullptr);
-  b->Scale(3);
-  b->RotateY(f*TAU);
-  b->Translate(0, -4, 4.5);  
-  // scene->add(b);
+  c = new Cylinder(new Lambertian(Colour(0, 1, 0)));
+  c->Scale(2, 2, 4);
+  // scene->add(c);
 
+  d = new Cylinder(new Lambertian(Colour(0, 1, 0)));
+  d->Scale(2, 2, 4);
+  d->RotateY(PI/2);
 
-  s = new CSGObject(CSGType::Difference, b, a);
-  s->bsdf = new Transmissive(1.47, Colour(0.5, 1, 0.5));
+  e = new Cylinder(new Lambertian(Colour(0, 1, 0)));
+  e->Scale(2, 2, 4);
+  e->RotateX(PI/2);
+
+  s = new CSGObject(Difference,
+        new CSGObject(Intersection, a, b),
+        new CSGObject(Union,
+          new CSGObject(Union, c, d),
+          e
+        )
+      );
+  s->RotateX(frameRatio*TAU);
+  s->RotateZ(frameRatio*PI);
+  // s->bsdf = new Transmissive(1.47, Colour(1));
+  s->Translate(0, 0, 5);
   scene->add(s);
 
   s = new Plane(new Emitter(Colour(12, 12, 12)));
